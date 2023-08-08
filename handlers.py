24b9c09f3490a7d0a86858e355b27bc3ad86f299
Ershov Timofey <eqeq.tima@yandex.ru>
2023-08-08 15:29:02 +0500

    v 1.0

--- handlers.py
+++ handlers.py
@@ -1,11 +1,8 @@
 import datetime
 import json
+from typing import Union
 
 from ru.travelfood.simple_ui import NoSQL as noClass
-
-
-db_all_birds = noClass("birds_2212")
-db_birds_which_i_saw = noClass("birds_which_i_saw")
 
 
 class hashMap:
@@ -37,6 +34,41 @@ class hashMap:
 
         pass
 
+
+class DB:
+    def get(self, key: str) -> str:
+        """
+        Получение значения по ключу
+        """
+
+        pass
+
+    def put(self, key: str, value: Union[str, bool, int], register: bool):
+        """
+        Помещение значения в указанный ключ
+        """
+
+        pass
+
+    def delete(self, key: str):
+        """
+        Удаление ключа
+        """
+
+        pass
+
+    def getallkeys(self) -> str:
+        """
+        Получение всех ключей базы в виде строки формата JSON-массива строк
+        """
+
+        pass
+
+
+db_all_birds: DB = noClass("birds")
+db_birds_which_i_saw: DB = noClass("birds_which_i_saw")
+
+
 #################################################
 # -------------- Процесс "Птицы" -------------- #
 
@@ -44,7 +76,7 @@ class hashMap:
 # ////// Экраны ////// #
 
 
-def show_birds(hashMap: hashMap, _files=None, _data=None):
+def show_birds_screen(hashMap: hashMap, _files=None, _data=None):
     """
     Экран, который запускается при запуске процесса "Птицы"
     """
@@ -84,7 +116,7 @@ def show_birds(hashMap: hashMap, _files=None, _data=None):
                             "BackgroundColor": "",
                             "width": "match_parent",
                             "height": "wrap_content",
-                            "weight": 2
+                            "weight": 1
                         },
                         {
                             "type": "LinearLayout",
@@ -98,25 +130,31 @@ def show_birds(hashMap: hashMap, _files=None, _data=None):
                                     "show_by_condition": "",
                                     "Value": "@bird_name",
                                     "NoRefresh": False,
+                                    "TextSize": "16",
+                                    "TextColor": "#ca6702",
                                     "document_type": "",
                                     "mask": "",
-                                    "Variable": ""
+                                    "Variable": "",
+                                    "gravity_horizontal": "left",
                                 },
                                 {
                                     "type": "TextView",
                                     "show_by_condition": "",
                                     "Value": "@bird_color",
                                     "NoRefresh": False,
+                                    "TextSize": "16",
+                                    "TextColor": "#9b2226",
                                     "document_type": "",
                                     "mask": "",
-                                    "Variable": ""
+                                    "Variable": "",
+                                    "gravity_horizontal": "left",
                                 },
                             ]
                         },
                     ]
                 },
             ]
-         }
+        }
         }
     }
 
@@ -125,23 +163,21 @@ def show_birds(hashMap: hashMap, _files=None, _data=None):
     keys_str = db_all_birds.getallkeys()
     keys = json.loads(str(keys_str).encode("utf-8"))
     for key in keys:
-        bird_data_str = db_all_birds.get(key)
-        bird_data = json.loads(bird_data_str)
-        obj = {
-            "key": key,
-            "bird_name": key,
-            "bird_color": bird_data.get("bird_color"),
-            "bird_picture": bird_data.get("bird_picture"),
-        }
-        birds_cards["customcards"]["cardsdata"].append(obj)
-    hashMap.put("cards", json.dumps(birds_cards, ensure_ascii=False).encode('utf8').decode())
+        bird = get_bird_data_by_key(key=key)
+        bird["bird_name"] = "Название птицы: " + key
+        bird["bird_color"] = "Цвет перьев птицы: " + bird.get("bird_color")
+        birds_cards["customcards"]["cardsdata"].append(bird)
+    hashMap.put(
+        "cards",
+        json.dumps(birds_cards, ensure_ascii=False)
+    )
     return hashMap
 
 
 # ////// Обработчики кнопок ////// #
 
 
-def show_add_bird_screen(hashMap, _files=None, _data=None):
+def show_add_bird_screen(hashMap: hashMap, _files=None, _data=None):
     """
     Запуск экрана с добавление птицы
     """
@@ -157,17 +193,30 @@ def create_bird(hashMap: hashMap, _files=None, _data=None):
 
     bird_name = hashMap.get("bird_name")
 
-    # проверка на уникальность...
+    # проверка на вхождение в базу
+    if db_all_birds.get(bird_name):
+        hashMap.put("toast", "Птица с данным названием уже внесена в базу.")
+        return hashMap
 
     bird_color = hashMap.get("bird_color")
 
+    if bird_name == "" or bird_color == "":
+        hashMap.put("toast", "Поля название и цвет являются обязательными")
+        return hashMap
+
     bird_data = {
         "bird_color": bird_color,
-        "bird_picture": None,
+        "bird_picture": hashMap.get("photo"),
     }
-    db_all_birds.put(bird_name, json.dumps(bird_data, ensure_ascii=False), True)
+    db_all_birds.put(
+        bird_name,
+        json.dumps(bird_data, ensure_ascii=False),
+        True
+    )
 
-    hashMap.put("toast", f"Птица успешно создана. {bird_name.upper()}")
+    hashMap.put("toast", f"Птица {bird_name} успешно создана.")
+
+    remove_bird_info_from_hashMap(hashMap=hashMap)
 
     return hashMap
 
@@ -179,43 +228,85 @@ def show_bird_screen(hashMap: hashMap, _files=None, _data=None):
 
     hashMap.put("ShowScreen", "Птица")
     bird_name = hashMap.get("selected_card_key")
-    # повтор
-    bird_data_str = db_all_birds.get(bird_name)
-    bird_data = json.loads(bird_data_str)
-    bird_color = bird_data.get("bird_color")
-    bird_picture = bird_data.get("bird_picture")
 
-    hashMap.put("bird_name", str(bird_name))
-    hashMap.put("bird_color", str(bird_color))
+    bird_data = get_bird_data_by_key(key=bird_name)
+
+    hashMap.put("bird_name", bird_data.get("bird_name"))
+    hashMap.put("bird_color", bird_data.get("bird_color"))
+    hashMap.put("bird_picture", bird_data.get("bird_picture"))
+
     return hashMap
 
 
-def destroy_all_keys(hashMap: hashMap, _files=None, _data=None):
+def delete_bird(hashMap: hashMap, _files=None, _data=None):
     """
-    Удаление всех птиц из базы данных "db_all_birds"
+    Удаление птицы из базы данных "db_all_birds"
     """
 
-    db_all_birds.destroy()
-    hashMap.put("FinishProcess", "Птицы")
-    hashMap.put("toast", "Все ключи базы данных удалены.")
+    bird_name = hashMap.get("selected_card_key")
+    db_all_birds.delete(bird_name)
+    hashMap.put("toast", f"Птица {bird_name} удалена из БД.")
+    hashMap.put("ShowScreen", "Список птиц")
+
+    remove_bird_info_from_hashMap(hashMap=hashMap)
+
     return hashMap
 
 
 def add_bird_which_i_saw_in_hashMap(hashMap: hashMap, _files=None, _data=None):
     """
-    Добавление птицы, которую "я" видел в хешмапу
+    Добавление птицы, которую "я" видел в глобальную переменную в hashMap
     """
 
-    if hashMap.containsKey("_birds_which_i_saw"):
+    if (
+        hashMap.containsKey("_birds_which_i_saw") and
+        hashMap.get("_birds_which_i_saw") is not None
+    ):
         birds_which_i_saw = hashMap.get("_birds_which_i_saw")
         birds_which_i_saw += "," + hashMap.get("selected_card_key")
         hashMap.put("_birds_which_i_saw", birds_which_i_saw)
     else:
         hashMap.put("_birds_which_i_saw", hashMap.get("selected_card_key"))
-    hashMap.put("toast", f"Птица: {hashMap.get('selected_card_key')} добавлена в список")
+
+    hashMap.put(
+        "toast",
+        f"Птица: {hashMap.get('selected_card_key')} добавлена в список"
+    )
     return hashMap
 
 # -------------------------------------------- #
+
+# ANY
+
+
+def get_bird_data_by_key(key: str) -> dict:
+    """
+    Получение данных о птице из db_all_birds
+    """
+
+    bird_data_str = db_all_birds.get(key)
+    bird_data = json.loads(bird_data_str)
+    bird = {
+        "key": key,
+        "bird_name": key,
+        "bird_color": bird_data.get("bird_color"),
+        "bird_picture": bird_data.get("bird_picture"),
+    }
+    return bird
+
+
+def remove_bird_info_from_hashMap(hashMap: hashMap):
+    """
+    Удаление информации о птице из hashMap
+    """
+
+    hashMap.remove("bird_name")
+    hashMap.remove("bird_color")
+    hashMap.remove("photo")
+
+    return hashMap
+
+
 ################################################
 
 
@@ -259,14 +350,10 @@ def show_birds_which_i_see_screen(hashMap: hashMap, _files=None, _data=None):
                             "document_type": "",
                             "mask": "",
                             "Variable": "",
-                            "TextSize": "16",
-                            "TextColor": "#DB7093",
-                            "TextBold": True,
-                            "TextItalic": False,
                             "BackgroundColor": "",
                             "width": "match_parent",
                             "height": "wrap_content",
-                            "weight": 2
+                            "weight": 1
                         },
                         {
                             "type": "LinearLayout",
@@ -280,36 +367,48 @@ def show_birds_which_i_see_screen(hashMap: hashMap, _files=None, _data=None):
                                     "show_by_condition": "",
                                     "Value": "@bird_name",
                                     "NoRefresh": False,
+                                    "TextSize": "16",
+                                    "TextColor": "#ca6702",
                                     "document_type": "",
                                     "mask": "",
-                                    "Variable": ""
+                                    "Variable": "",
+                                    "weight": "",
+                                    "gravity_horizontal": "left",
                                 },
                                 {
                                     "type": "TextView",
                                     "show_by_condition": "",
                                     "Value": "@count_saw",
                                     "NoRefresh": False,
+                                    "TextSize": "16",
+                                    "TextColor": "#9b2226",
                                     "document_type": "",
                                     "mask": "",
-                                    "Variable": ""
+                                    "Variable": "",
+                                    "gravity_horizontal": "left",
+
                                 },
                                 {
                                     "type": "TextView",
                                     "show_by_condition": "",
+                                    "TextColor": "#4682B4",
                                     "Value": "@updated_date",
                                     "NoRefresh": False,
                                     "document_type": "",
                                     "mask": "",
-                                    "Variable": ""
+                                    "Variable": "",
+                                    "gravity_horizontal": "left",
                                 },
                                 {
                                     "type": "TextView",
                                     "show_by_condition": "",
+                                    "TextColor": "#4682B4",
                                     "Value": "@created_date",
                                     "NoRefresh": False,
                                     "document_type": "",
                                     "mask": "",
-                                    "Variable": ""
+                                    "Variable": "",
+                                    "gravity_horizontal": "left",
                                 },
                             ]
                         },
@@ -329,14 +428,19 @@ def show_birds_which_i_see_screen(hashMap: hashMap, _files=None, _data=None):
         bird_data = json.loads(bird_data_str)
         obj = {
             "key": key,
-            "bird_name": key,
+            "bird_name": "Название птицы: " + key,
             "bird_picture": bird_data.get("bird_picture"),
-            "count_saw": bird_data.get("count_saw"),
-            "updated_date": bird_data.get("updated_date"),
-            "created_date": bird_data.get("created_date"),
+            "count_saw": "Видел раз: " + str(bird_data.get("count_saw")),
+            "updated_date": (
+                "Дата обновления: " + bird_data.get("updated_date")
+            ),
+            "created_date": "Дата создания: " + bird_data.get("created_date"),
         }
         birds_cards["customcards"]["cardsdata"].append(obj)
-    hashMap.put("cards", json.dumps(birds_cards, ensure_ascii=False).encode('utf8').decode())
+    hashMap.put(
+        "cards",
+        json.dumps(birds_cards, ensure_ascii=False)
+    )
     return hashMap
 
 
@@ -348,41 +452,65 @@ def create_birds_which_i_saw(hashMap: hashMap, _files=None, _data=None):
     Внесение птицы, которую(-ых) я видел в БД "db_birds_which_i_saw"
     """
 
-    if hashMap.containsKey("_birds_which_i_saw") and hashMap.get("_birds_which_i_saw") is not None:
+    if (
+        hashMap.containsKey("_birds_which_i_saw") and
+        hashMap.get("_birds_which_i_saw") is not None
+    ):
         birds_which_i_saw = hashMap.get("_birds_which_i_saw").split(",")
     else:
         hashMap.put("toast", "Список птиц на добавление пуст")
         return hashMap
 
     for bird in birds_which_i_saw:
-        bird_data_str = db_birds_which_i_saw.get(bird)
-        if bird_data_str:
-            bird_data = json.loads(bird_data_str)
-            new_bird_data = {
-                "updated_date": datetime.datetime.now().strftime("%d-%m-%Y %H:%M"),
-                "created_date": bird_data.get("created_date"),
-                "bird_name": bird,
-                "bird_picture": bird_data.get("bird_picture"),
-                "count_saw": int(bird_data.get("count_saw")) + 1
-            }
-        else:
-            bird_data_str = db_all_birds.get(bird)
-            bird_data = json.loads(bird_data_str)
-            bird_picture = bird_data.get("bird_picture")
+        new_bird_data = get_bird_data_for_saw(bird)
+        db_birds_which_i_saw.put(
+            bird,
+            json.dumps(new_bird_data, ensure_ascii=False),
+            True
+        )
 
-            new_bird_data = {
-               "updated_date": datetime.datetime.now().strftime("%d-%m-%Y %H:%M"),
-               "created_date": datetime.datetime.now().strftime("%d-%m-%Y %H:%M"),
-               "bird_name": bird,
-               "bird_picture": bird_picture,
-               "count_saw": 1,
-            }
-        db_birds_which_i_saw.put(bird, json.dumps(new_bird_data, ensure_ascii=False), True)
-    hashMap.put("toast", hashMap.get("_birds_which_i_saw"))
-    # удалить из hashMap remove не работает??? можно указать значение None = костыль
+    _birds_which_i_saw = hashMap.get("_birds_which_i_saw")
+    hashMap.put("toast", f"Добавлены птицы: {_birds_which_i_saw}")
+
+    # удалить из hashMap remove не работает для глобальных???
+    # указал значение None = костыль
     hashMap.put("_birds_which_i_saw", None)
 
     return hashMap
+
+
+# ANY
+
+def get_bird_data_for_saw(bird: str) -> dict:
+    """
+    Получение данных о птице из db_birds_which_i_saw
+    """
+
+    bird_data_str = db_birds_which_i_saw.get(bird)
+    if bird_data_str:
+        bird_data = json.loads(bird_data_str)
+    else:
+        bird_data_from_dab = db_all_birds.get(bird)
+        bird_data = json.loads(bird_data_from_dab)
+
+    updated_date = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
+    created_date = (
+        bird_data.get("created_date") if bird_data_str
+        else datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
+    )
+    bird_pciture = bird_data.get("bird_picture")
+    count_saw = int(bird_data.get("count_saw")) + 1 if bird_data_str else 1
+
+    new_bird_data = {
+        "updated_date": updated_date,
+        "created_date": created_date,
+        "bird_name": bird,
+        "bird_picture": bird_pciture,
+        "count_saw": count_saw
+    }
+
+    return new_bird_data
+
 
 # -------------------------------------------------- #
 ######################################################
